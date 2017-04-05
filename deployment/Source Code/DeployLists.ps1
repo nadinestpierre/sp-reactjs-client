2384c9d2461b06d7d0e405b95899b81c5aeced9f
Dmitry Dyachenko <ddyachenko@golin.com>
2017-04-05 15:41:27 +0100

    feat(): Updated PnP Scripts

--- deployment/Source Code/DeployLists.ps1
+++ deployment/Source Code/DeployLists.ps1
@@ -3,7 +3,8 @@
     [Parameter(Mandatory = $true)]
     [string]$Credential,
     [Parameter(Mandatory = $true)]
-    [string]$RootLocation 
+    [string]$RootLocation,
+    [string]$SubSite
 )
 
 $LogFilePath = "$RootLocation\DeployListsLog.txt"
@@ -14,12 +15,20 @@ $ErrorActionPreference = "Stop"
 #------------------------------------------------------------------
 
 Try {
-    Connect-SPOnline -Url $SiteUrl -Credentials $Credential
+    Connect-PnPOnline -Url $SiteUrl -Credentials $Credential
 
+    $webName = ''
+
+    if($SubSite) {
+        $webName = $SubSite -replace '\s',''
+        $webName = $webName -replace '/',''
+        $webName = '_' + $webName
+    } 
+    
     Import-Module "$RootLocation\Modules\CreateLists.psm1"    
-    CreateLists -inputFile "$RootLocation\Content\Lists\Lists.xml" -RootLocation $RootLocation -recreate $false -debug $false
+    CreateLists -inputFile "$RootLocation\Content\Lists\Lists" + $webName + ".xml" -RootLocation $RootLocation -SubSite $SubSite -recreate $false -debug $false
 
-    Disconnect-SPOnline
+    Disconnect-PnPOnline
 }
 Catch {
     $DateTime = Get-Date
